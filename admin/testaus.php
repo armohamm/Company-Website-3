575738e17e21463d80385e2519e6b7ab51464729
awttu <murto.arttu@gmail.com>
2019-11-14 16:31:42 +0200

    jatketaan joskus

--- admin/testaus.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types =1);
-include 'includes/class-autoloader.inc.php';
-
-$objekti = new Reference();
-$array = $objekti->getReferences();
-$pos = $objekti->getRefHeading(3);
-print_r($array[3][$pos]);
